5ce5782bfe90193dc96f54eb9361ea0f1d23e721
hyeonji <hyeonji.27@daum.net>
2021-02-24 14:04:41 +0900

    예산

---
+++ programmers/src/Level1/Budget.java
@@ -0,0 +1,23 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class Budget {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+
+        for(int i=0; i<d.length; i++) {
+            if(budget-d[i] >= 0) {
+                budget -= d[i];
+                answer++;
+            } else break;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Budget b = new Budget();
+        System.out.println(b.solution(new int[] {1, 3, 2, 5, 4}, 9));
+    }
+}
