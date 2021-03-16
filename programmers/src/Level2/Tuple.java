89e13d22e338faee1db00c4f2fbb832e19de25d2
hyeonji <hyeonji.27@daum.net>
2021-03-16 19:18:37 +0900

    튜플

---
+++ programmers/src/Level2/Tuple.java
@@ -0,0 +1,40 @@
+package Level2;
+
+import java.util.*;
+
+public class Tuple {
+    public int[] solution(String s) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        String str[] = s.replaceAll("[{}]", "").split(",");
+
+        for(int i=0; i<str.length; i++) {
+            if(hm.containsKey(str[i])) {
+                int count = hm.get(str[i]);
+                hm.put(str[i], count+1);
+            } else {
+                hm.put(str[i], 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(hm.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue()-o1.getValue();
+            }
+        });
+
+        int []answer = new int[list.size()];
+        int idx = 0;
+        for(Map.Entry<String, Integer> entry : list) {
+            answer[idx++] = Integer.parseInt(entry.getKey());
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Tuple tuple = new Tuple();
+        int arr[] = tuple.solution("{20,111},{111}");
+        for(int num: arr) System.out.println(num);
+    }
+}
