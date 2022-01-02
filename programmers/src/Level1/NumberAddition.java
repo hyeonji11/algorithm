0cbfdce056aaec3a387cceefe589826bed96d89d
hyeonji <hyeonji.27@daum.net>
2022-01-02 15:51:17 +0900

    음양 더하기

---
+++ programmers/src/Level1/NumberAddition.java
@@ -0,0 +1,15 @@
+package Level1;
+
+public class NumberAddition {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; i++) {
+            if(signs[i]) answer += absolutes[i];
+            else answer += absolutes[i]*-1;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        NumberAddition test = new NumberAddition();
+    }
+}
