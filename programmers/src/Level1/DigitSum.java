4155a678433cb6e774b02908c7f9870bca9ae9fa
hyeonji <hyeonji.27@daum.net>
2022-01-06 22:25:15 +0900

    자릿수 더하기

---
+++ programmers/src/Level1/DigitSum.java
@@ -0,0 +1,17 @@
+package Level1;
+
+public class DigitSum {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
