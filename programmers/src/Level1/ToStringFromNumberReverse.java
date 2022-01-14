276ea12de73bf6b6b01d541d5a60bda92815c1d7
hyeonji <hyeonji.27@daum.net>
2022-01-14 21:24:57 +0900

    자연수 뒤집어 배열로 만들기

---
+++ programmers/src/Level1/ToStringFromNumberReverse.java
@@ -0,0 +1,19 @@
+package Level1;
+
+public class ToStringFromNumberReverse {
+    public int[] solution(long n) {
+        String str = ""+n;
+        int len = str.length();
+        int[] answer = new int[len];
+
+        for(int i=1; i<=str.length(); i++) {
+            answer[len-i] = str.charAt(i-1)-'0';
+            //System.out.println(answer[len-i]);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        ToStringFromNumberReverse test = new ToStringFromNumberReverse();
+        test.solution(12345);
+    }
+}
