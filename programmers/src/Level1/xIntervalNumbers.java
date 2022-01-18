cff137464095b645e12feda674b842d259f00ca7
hyeonji <hyeonji.27@daum.net>
2022-01-18 20:19:34 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ programmers/src/Level1/xIntervalNumbers.java
@@ -0,0 +1,18 @@
+package Level1;
+
+public class xIntervalNumbers {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        for(int i=0; i<n; i++) {
+            answer[i] = num;
+            num += x;
+            //System.out.println(answer[i]);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        xIntervalNumbers xin = new xIntervalNumbers();
+        xin.solution(-4, 2);
+    }
+}
