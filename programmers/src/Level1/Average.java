ad5b6f1f5a6ed61963f4533bb2a4483433913673
hyeonji <hyeonji.27@daum.net>
2022-01-20 22:48:25 +0900

    평균 구하기

---
+++ programmers/src/Level1/Average.java
@@ -0,0 +1,16 @@
+package Level1;
+
+public class Average {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        for(int i=0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        answer = (double)sum/arr.length;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
