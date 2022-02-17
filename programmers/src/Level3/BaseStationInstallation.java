1242650cc9bb688d03d0dfbbc26f65b33ef910cc
hyeonji <hyeonji.27@daum.net>
2022-02-17 23:39:32 +0900

    기지국 설치 하는중

---
+++ programmers/src/Level3/BaseStationInstallation.java
@@ -0,0 +1,18 @@
+package Level3;
+
+public class BaseStationInstallation {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        boolean[] apartment = new boolean[n];
+
+        for(int i=0; i<stations.length; i++) {
+            int idx = stations[i]-1;
+
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
