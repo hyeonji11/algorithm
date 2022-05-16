074c29f7f931cb75ce3406c09f2460a7624c4bd9
hyeonji11 <hyeonji.27@daum.net>
2022-05-16 19:58:15 +0900

    ZOAC 수정

--- baekjoon/src/implementation/P16719.java
+++ baekjoon/src/implementation/P16719.java
@@ -3,31 +3,29 @@ package implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Arrays;
 
 public class P16719 {
+    public static void zoac(String[] str, int start, int end, String[] result) {
+        if(start == end) return;
+        int minIdx = start;
+        for(int i=start+1; i<end; i++) {
+            if(str[minIdx].charAt(0) >  str[i].charAt(0)) {
+                minIdx = i;
+            }
+        }
+        result[minIdx] = str[minIdx];
+        System.out.println(String.join("", result));
+        zoac(str, minIdx+1, end, result);
+        zoac(str, start, minIdx, result);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        Map<Integer, Integer> map = new TreeMap<>();
-        String[] strArr = new String[str.length()];
-        StringBuilder sb;
+        String[] str = br.readLine().split("");
+        String[] result = new String[str.length];
+        Arrays.fill(result, "");
 
-        for(int i=0; i<str.length(); i++) {
-            map.put((int)str.charAt(i), i);
-        }
+        zoac(str, 0, str.length, result);
 
-        for(Integer val : map.keySet()) {
-            sb = new StringBuilder("");
-            int idx = map.get(val);
-            strArr[idx] = str.substring(idx, idx+1);
-            for(String string: strArr) {
-                if(string != null)
-                    sb.append(string);
-            }
-
-            System.out.println(sb.toString());
-        }
     }
 }
