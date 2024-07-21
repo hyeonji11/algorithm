4be050d5608e1d3dc9918780618f1ffc11d5ba32
hyeonji11 <hyeonji.27@daum.net>
2024-07-21 09:54:27 +0900

    OX퀴즈

---
+++ baekjoon/src/string/P8958.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<testCase; i++) {
+            String str = br.readLine();
+            int cnt = 0;
+            int sum = 0;
+            for(int j=0; j<str.length(); j++) {
+                if(str.charAt(j) == 'O') {
+                    cnt++;
+                    sum += cnt;
+                } else {
+                    cnt = 0;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
