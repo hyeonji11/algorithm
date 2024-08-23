2d0d5f5e5007bafea0744ddb170570465633d10b
hyeonji11 <hyeonji.27@daum.net>
2024-08-23 18:36:52 +0900

    별 찍기 - 8

--- baekjoon/src/implementation/P2445.java
+++ baekjoon/src/implementation/P2445.java
@@ -1,2 +1,47 @@
-package implementation;public class P2445 {
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2445 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int total = num*2;
+
+        StringBuilder sb = new StringBuilder();
+
+        int cnt;
+        for(int i=1; i<=num; i++) {
+            cnt = total - i*2;
+
+            for(int j=0; j<i; j++) {
+                sb.append("*");
+            }
+            for(int j=0; j<cnt; j++) {
+                sb.append(" ");
+            }
+            for(int j=0; j<i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for(int i=num-1; i>0; i--) {
+            cnt = total - i*2;
+
+            for(int j=0; j<i; j++) {
+                sb.append("*");
+            }
+            for(int j=0; j<cnt; j++) {
+                sb.append(" ");
+            }
+            for(int j=0; j<i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
