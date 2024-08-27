5961d83d7817c51fcf8acca02cdabcd97713afa0
hyeonji11 <hyeonji.27@daum.net>
2024-08-27 16:26:33 +0900

    별 찍기 - 15

---
+++ baekjoon/src/implementation/P10990.java
@@ -0,0 +1,27 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10990 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int space;
+        int front = num-1;
+        sb.append(" ".repeat(front--));
+        sb.append("*").append("\n");
+        for(int i=2; i<=num; i++) {
+            space = (i-1)*2-1;
+            sb.append(" ".repeat(front--));
+            sb.append("*");
+            sb.append(" ".repeat(space));
+            sb.append("*").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
