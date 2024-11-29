4d39d2d5c918347d69f1649e96bcb94e83d443a3
hyeonji11 <hyeonji.27@daum.net>
2024-11-29 22:42:06 +0900

    별찍기 - 2

---
+++ baekjoon/src/implementation/P2439.java
@@ -0,0 +1,19 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=num; i++) {
+            sb.append(" ".repeat(num-i)).append("*".repeat(i)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
