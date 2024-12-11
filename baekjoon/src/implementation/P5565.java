b4f7141fb6faf414eb7ff94536bc0eaeddf04212
hyeonji11 <hyeonji.27@daum.net>
2024-12-11 15:28:30 +0900

    영수증

---
+++ baekjoon/src/implementation/P5565.java
@@ -0,0 +1,17 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P5565 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<9; i++) {
+            total -= Integer.parseInt(br.readLine());
+        }
+        System.out.println(total);
+    }
+}
