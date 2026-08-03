36372bc391e53ec40a46f9d327e573711dbb0094
hyeonji11 <hyeonji.27@daum.net>
2026-08-03 15:20:09 +0900

    328. Odd Even Linked List

---
+++ leetcode/src/LinkedList/OddEvenLinkedList.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class OddEvenLinkedList {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenStart = even;
+
+        while(odd.next != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenStart;
+
+        return head;
+    }
+}
